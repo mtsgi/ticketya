ab516aeb8dea6a1c2d4d16e955f37ae48f0d581a
ne290045 <ne290045@senshu-u.jp>
2018-12-11 17:46:43 +0900

    authメソッドの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
     validates( :name, presence: true )
     validates( :tel, presence: true, numericality: true, uniqueness: true )
     validates( :password, presence: true, confirmation: { allow_blank: true } )
+
+    class << self
+        def auth( tel, password )
+            user = find_by( tel: tel )
+            if( user && user.password == password )
+                user
+            else nil end
+        end
+    end
 end
